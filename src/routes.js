7464057e207d8448b200e0bea07ea30ddf7cff15
Stiliyanss <stilianss03@gmail.com>
2025-02-10 23:52:19 +0200

    add create controller and page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import homeController from './controllers/home-controllers.js';
+import movieController from './controllers/movie-controller.js';
 
 const routes = express.Router();
 
 routes.use(homeController);
+routes.use(movieController);
 
 export default routes;
