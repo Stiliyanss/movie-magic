648fb9088170d25b807d3cdb83a767f91aa95447
Stiliyanss <stilianss03@gmail.com>
2025-02-11 00:13:28 +0200

    add details page with hardcoded data

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -6,4 +6,9 @@ movieController.get('/create', (req,res)=>{
   res.render('create');
 })
 
+movieController.get('/:movieId/details', (req,res)=>{
+  let id = req.params.movieId;
+  res.render('details');
+})
+
 export default movieController;
