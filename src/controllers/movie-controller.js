bc3672b943ddd6d8e1a858db783e4d4a36912471
Stiliyanss <stilianss03@gmail.com>
2025-02-11 14:11:06 +0200

    parse form from data on server

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -5,13 +5,19 @@ const movieController = express.Router();
 
 movieController.get('/create', (req,res)=>{
   res.render('create');
+});
+
+movieController.post('/create', (req,res)=>{
+   const newMovie = req.body;
+
+   res.end();
 })
 
 movieController.get('/:movieId/details', (req,res)=>{
   let movieId = req.params.movieId;
   const movie = movieService.findOne(movieId); 
 
-  res.render('details');
+  res.render('details', {movie});
 })
 
 export default movieController;
