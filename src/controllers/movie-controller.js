0d1fd39fee7b2b1c7f6f87e154e9dbf65703989d
Stiliyanss <stilianss03@gmail.com>
2025-02-13 01:38:31 +0200

    Show movie data on attach page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -23,14 +23,16 @@ movieController.post('/create', async (req,res)=>{
 })
 
 movieController.get('/:movieId/details',async (req,res)=>{
-  let movieId = req.params.movieId;
+  const movieId = req.params.movieId;
   const movie = await movieService.getOne(movieId); 
 
   res.render('movie/details', {movie});
 })
 
-movieController.get('/:movieId/attach-cast', (req,res)=>{
-  res.render('movie/attach-cast')
+movieController.get('/:movieId/attach-cast', async (req,res)=>{
+  const movieId = req.params.movieId;
+  const movie = await movieService.getOne(movieId);
+  res.render('movie/attach-cast',{movie})
 })
 
 export default movieController;
