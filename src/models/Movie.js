812202a9f30e075bea394aaa9dfecc3da86271ed
Stiliyanss <stilianss03@gmail.com>
2025-02-12 17:21:44 +0200

    Create movie model

---
+++ src/models/Movie.js
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+
+//create schema
+const movieSchema = new Schema({
+  title: String,
+  category: String,
+  genre: String,
+  director: String,
+  year: Number,
+  imageUrl: String,
+  rating: Number,
+  description: String,  
+});
+
+//create model
+const Movie = model('Movie',movieSchema);
+
+
+//export model
+export default Movie;
