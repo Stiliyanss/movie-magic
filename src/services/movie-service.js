196c92269535e5f202f602359aa49917ca643494
Stiliyanss <stilianss03@gmail.com>
2025-02-15 15:11:47 +0200

    delete movie

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -34,16 +34,20 @@ export default {
     return query;
   },
 
-  async attachCast(movieId,castId){
+   attachCast(movieId,castId){
     //1st way to attach
-    const movie = await Movie.findById(movieId);
+    const movie =  Movie.findById(movieId);
     movie.casts.push(castId);
-    await movie.save();
+     movie.save();
 
     return movie;
     //2nd way to attach
 
     //return Movie.findByIdAndUpdate(movieId, {$push: {casts:castId}})
+  },
+
+  delete(movieId){
+      return Movie.findByIdAndDelete(movieId);
   }
 
 }
