135e97ed89e16562bf112386bdac86b34fdf3a77
Stiliyanss <stilianss03@gmail.com>
2025-02-15 12:13:48 +0200

    Add login functionality with jwt generation

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -17,4 +17,20 @@ authController.get('/login', (req,res)=>{
   res.render('./auth/login');
 });
 
+authController.post('/login', async(req,res)=>{
+  const {email,password} = req.body;
+
+  try {
+    const token = await authService.login(email,password);
+    console.log(token);
+    
+
+  } catch (error) {
+    console.log(error.message);
+    return res.redirect('/404');
+  }
+
+  res.redirect('/')
+})
+
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,33 @@
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = 'rf4c3gt54VHTRH>>/v.vtqrtre'
 
 export default {
   register(userData){
     return User.create(userData);    
+  },
+  async login(email,password){
+    const user = await User.findOne({email});
+        //check user exists
+    if(!user){
+      throw new Error('Invalid email or password');
+    }
+    //check password is correct
+    const isValid = await bcrypt.compare(password,user.password); 
+    if(!isValid){
+      throw new Error('Invalid email or password'); 
+    }
+    //generate token
+    const payload = {
+      id: user.id,
+      email: user.email
+    }
+    const token = jwt.sign(payload, SECRET, {expiresIn:'2h'});
+
+    //return token
+    return token;
   }
+
 }
