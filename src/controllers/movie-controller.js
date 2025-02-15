4daebe25bc8959fbba2224f17cfe27251194d03f
Stiliyanss <stilianss03@gmail.com>
2025-02-15 15:31:49 +0200

    add edit page with hardcoded data

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -58,6 +58,10 @@ movieController.get('/:movieId/delete', async(req,res)=> {
 
    await movieService.delete(movieId);
    res.redirect('/');
+});
+
+movieController.get('/:movieId/edit', (req,res)=>{
+  res.render('movie/edit');
 })
 
 export default movieController;
